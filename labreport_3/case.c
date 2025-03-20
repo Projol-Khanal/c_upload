c5d134964b57b160f1064148a968ba0176a72de2
Projol-Khanal <khanalprojol017@gmail.com>
2025-03-20 21:21:31 +0545

    switch case operators

--- labreport_3/case.c
+++ labreport_3/case.c
@@ -1,26 +1,28 @@
 // WAP to perform the operations like addition, subtraction, multiplication and division on two numbers on the basis of user choice using switch case.
 #include <stdio.h>
+// #include <conio.h>
 int main() 
 {
-    int a,b, choice, result;
+    int a,b,ch;
     printf("Enter any two numbers: ");
     scanf("%d %d", &a, &b);
-    printf("\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division");
-    printf("\nEnter your choice: ");
-    scanf("%d", &choice);
-    switch (choice) 
+    printf("enter any of the operators among +,-,* and /: ");
+    // fflush(stdin);
+    ch=getchar();
+    switch (ch) 
     {
-    case 1:
+    case '+':
         
-    printf("Addition of %d and %d is %d", a, b, a+b);
+    printf("Sum of %d and %d is %d", a, b, a+b);
     break;
-    case 2:
-    printf("Subtraction of %d and %d is %d", a, b, a-b);
+
+    case '-':
+    printf("Difference of %d and %d is %d", a, b, a-b);
     break;
-    case 3:
-    printf("Multiplication of %d and %d is %d", a, b, a*b);
+    case '*':
+    printf("Product of %d and %d is %d", a, b, a*b);
     break;
-    case 4:
+    case '/':
     
     printf("Division of %d and %d is %d", a, b, a/b);
     break;
